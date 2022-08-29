22ce878541682613298ad9fafaa621e04a193c3a
rob-ked <rob.kedzierski@gmail.com>
2022-08-29 18:56:54 +0200

    Klasa zapewniająca obsługę pliku konfiguracyjnego.

---
+++ CGHangfireApp/Helper/Settings.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CGHangfireApp.Helper
+{
+    public sealed class Settings
+    {
+        private static Model.Settings _params;
+        public static Model.Settings Params
+        {
+            get
+            {
+                if (_params == null)
+                {
+                    ProcessSettingsFile();
+                }
+
+                return _params;
+            }
+        }
+
+        private Settings()
+        {
+
+        }
+
+        /// <summary>
+        /// Przetwarza plik ustawień aplikacji
+        /// </summary>
+        private static void ProcessSettingsFile()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("Settings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            _params = config.GetRequiredSection("Settings").Get<Model.Settings>();
+        }
+    }
+}
